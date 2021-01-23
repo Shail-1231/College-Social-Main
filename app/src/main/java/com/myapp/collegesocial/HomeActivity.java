696dc5cc4b4e22bbed189abf55c336c60edd2f48
Shail-1231 <shaileng181@gmail.com>
2021-01-23 13:06:07 +0530

    Second Test

--- app/src/main/java/com/myapp/collegesocial/HomeActivity.java
+++ app/src/main/java/com/myapp/collegesocial/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.myapp.collegesocial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.widget.GridView;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
@@ -11,19 +16,35 @@ public class HomeActivity extends AppCompatActivity {
 
     GridView gridView;
     String[] colleges = {"Ahmedabad University", "Nirma University", "LJ University", "PDPU"};
-    int[] images = {R.drawable.aublur, R.drawable.nirmablur, R.drawable.ljblur, R.drawable.pdpublur};
+    int[] blurImages = {R.drawable.aublur, R.drawable.nirmablur, R.drawable.ljblur, R.drawable.pdpublur};
+    SliderView sliderView;
+    int[] mainEventImages = {R.drawable.convocation, R.drawable.ingenium};
+    SliderAdp sliderAdp;
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        sliderView = findViewById(R.id.slider_view);
+
+        sliderAdp = new SliderAdp(mainEventImages);
+
+        sliderView.setSliderAdapter(sliderAdp);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+
+        sliderView.startAutoCycle();
+
         gridView = findViewById(R.id.grid);
         ArrayList<CollegeModel> collegeModelArrayList = new ArrayList<CollegeModel>();
 
         for(int i = 0; i < colleges.length; i++)
         {
-            CollegeModel model = new CollegeModel(colleges[i], images[i]);
+            CollegeModel model = new CollegeModel(colleges[i], blurImages[i]);
             collegeModelArrayList.add(model);
         }
 
